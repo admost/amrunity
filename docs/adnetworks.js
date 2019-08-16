3d1c1d417fd83b539d3d250601a388f1c0de3b76
ataduru <ata_duru@hotmail.com>
2019-08-16 14:34:02 +0300

    small fix

--- docs/adnetworks.js
+++ docs/adnetworks.js
@@ -184,7 +184,7 @@ function downloadPackage()
   var form = document.createElement("form");
   form.target = "print_popup";
   form.method = "POST";
-  form.action = "http://localhost:62029/Main.aspx";
+  form.action = "http://download.admost.com/Main.aspx";
   form.style.display = "none";
   form.onsubmit=function(){window.open('about:blank','print_popup','width=1000,height=800')};
 
@@ -194,7 +194,7 @@ function downloadPackage()
   input.value = fileCont;
   form.appendChild(input);
 
-  var input2 = document.createElement("input2");
+  var input2 = document.createElement("input");
   input2.type = "hidden";
   input2.name = "hasCore";
   input2.value = uFlag;
